66b99f841322273eb1021062582cc2db4bd87467
Mathieu Leplatre <mathieu@mozilla.com>
2015-06-18 13:28:40 +0200

    Back to development: 1.1.1

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -3,6 +3,12 @@ Changelog
 
 This document describes changes between each past release.
 
+1.1.1 (unreleased)
+------------------
+
+- Nothing changed yet.
+
+
 1.1.0 (2015-06-18)
 ------------------
 
--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ ENTRY_POINTS = {}
 
 
 setup(name='cliquet-fxa',
-      version='1.1.0',
+      version='1.1.1.dev0',
       description='Firefox Accounts support in Cliquet',
       long_description=README + "\n\n" + CHANGELOG + "\n\n" + CONTRIBUTORS,
       license='Apache License (2.0)',
