d6d5aad8caf30555f1182069520e853073842045
Mathieu Leplatre <mathieu@mozilla.com>
2016-12-22 16:46:51 +0100

    Back to development: 2.3.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ ENTRY_POINTS = {}
 
 
 setup(name='kinto-fxa',
-      version='2.3.0',
+      version='2.3.1.dev0',
       description='Firefox Accounts support in Kinto',
       long_description=README + "\n\n" + CHANGELOG + "\n\n" + CONTRIBUTORS,
       license='Apache License (2.0)',
